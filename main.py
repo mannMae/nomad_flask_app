ddc720636dc92b534653d7b1c543e84c24403be1
김재만 <daga4242@gmail.com>
2023-01-24 18:15:08 +0900

    feat: search and export

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# from extractors.indeed import extract_indeed_jobs
-# from extractors.wwr import extract_wwr_jobs
-# from file import save_to_file
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 # keyword = input("Keyword?")
 
@@ -13,6 +13,9 @@ from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
+db = {
+}
+
 @app.route("/")
 def home():
     # return "<h1>HIHI</h1><a href='/hello'>go to hello</a>"
@@ -20,6 +23,26 @@ def home():
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        indeed = extract_indeed_jobs(keyword)
+        wwr = extract_wwr_jobs(keyword)
+        jobs = indeed + wwr
+        db[keyword] = jobs
+    return render_template("search.html", keyword = keyword, jobs=jobs)
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 app.run("")
